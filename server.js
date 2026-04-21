679d843b484cc54fb2eb37fc3266bf50f07c0c11
GAYATHRI RAJA <rajagayatriraja@gmail.com>
2026-04-21 23:29:27 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const studentRoutes = require("./routes/studentRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/students", studentRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
